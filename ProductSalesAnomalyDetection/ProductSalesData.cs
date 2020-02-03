14fea9b55ec9751a0c1363aa458547dd0d65614f
aakwah <aakwah@gmail.com>
2020-02-03 08:59:43 +0300

    Добавьте файлы проекта.

---
+++ ProductSalesAnomalyDetection/ProductSalesData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace ProductSalesAnomalyDetection
+{
+    public class ProductSalesData
+    {
+        [LoadColumn(0)]
+        public string Month;
+
+        [LoadColumn(1)]
+        public float numSales;
+    }
+
+    public class ProductSalesPrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(3)]
+        public double[] Prediction { get; set; }
+    }
+}
